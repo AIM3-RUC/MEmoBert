5010efdac017044f11b2f7180d5c7039281976f4
JinmingZhao <zhaojinming@ruc.edu.cn>
2021-05-05 18:37:27 +0000

    remove emo type for uniter3m

--- code/uniter3m/model/model.py
+++ code/uniter3m/model/model.py
@@ -211,6 +211,44 @@ class UniterPreTrainedModel(nn.Module):
                                    "\n\t".join(error_msgs)))
         return model
 
+class UniterTextEmbeddings(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        self.config = config
+
+        self.word_embeddings = nn.Embedding(config.vocab_size,
+                                            config.hidden_size, padding_idx=0)
+        # build position vocab embeddings = 512
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings,
+                                                config.hidden_size)
+        self.token_type_embeddings = nn.Embedding(config.type_vocab_size,
+                                                  config.hidden_size)      
+        print("[Warning] Donot use emo type embeddings add to input")
+        # self.LayerNorm is not snake-cased to stick with TensorFlow model
+        # variable name and be able to load any TensorFlow checkpoint file
+        self.LayerNorm = FusedLayerNorm(config.hidden_size, eps=1e-12)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+    def forward(self, input_ids, position_ids, token_type_ids=None):
+        '''
+        emo_type_ids: the emotion types of the input ids
+        batch-data
+        '''
+        if token_type_ids is None:
+            token_type_ids = torch.zeros_like(input_ids)
+
+        words_embeddings = self.word_embeddings(input_ids)
+        position_embeddings = self.position_embeddings(position_ids)
+        token_type_embeddings = self.token_type_embeddings(token_type_ids)
+        embeddings = (words_embeddings
+                      + position_embeddings
+                      + token_type_embeddings)
+                          
+        embeddings = self.LayerNorm(embeddings)
+        embeddings = self.dropout(embeddings)
+        return embeddings
+
 class UniterSpeechEmbeddings(nn.Module):
     def __init__(self, config, speech_dim):
         super().__init__()
