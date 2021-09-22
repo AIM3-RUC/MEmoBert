4c0aecb333bcb213c321872b3f9ec18b58043e18
JinmingZhao <zhaojinming@ruc.edu.cn>
2021-09-22 02:55:03 +0000

    some update

--- code/downstream/run.sh
+++ code/downstream/run.sh
@@ -10,9 +10,9 @@ do
     for i in `seq 1 10`;
     do
         cmd="python run_baseline.py --gpu_id $gpu --modality=$modality 
-            --pretained_ft_type denseface_openface_iemocap_mean_std_torch
+            --pretained_ft_type denseface_affectnet_openface_iemocap_mean_std_torch
             --num_threads 0 --cvNo=$i --run_idx=$run_idx
-            --dropout_rate 0.5 --postfix self
+            --dropout_rate 0.5 --postfix www
             --max_lexical_tokens 22
             --l_hidden_size 128 --v_hidden_size 128 --mid_fusion_layers '256,128'
         "
