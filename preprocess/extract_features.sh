1325f59ba3af6c50cc388763775d4d8c3e4b5ea3
JinmingZhao <zhaojinming@ruc.edu.cn>
2021-09-23 23:53:11 +0000

    some update

--- preprocess/extract_features.sh
+++ preprocess/extract_features.sh
@@ -3,10 +3,10 @@
 # can run at avec2230 docker, or transformers docker
 # 进程数据对应gpus的列表的数目，比如8个进程，gpus=(0 0 1 1 2 2 3 3)，每块卡分两个进程同时跑
 
-split_num=$1  # 8
+split_num=$1  # 12
 file_name=$2  # has_active_spk
 screen_name=MEmoBert_feature_extra
-gpus=(6 6 7 7)
+gpus=(1 1 2 2 3 3 4 4 5 5 6 6)
 for i in `seq 0 1 $(($split_num-1))`; 
     do
     {   
